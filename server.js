7f06290d49744d2d1cfe20d339eea87d6d17194a
Germano Kuerten <germanokuerten@gmail.com>
2022-09-29 23:58:12 -0700

    Use the Rest Operator with Function Parameters with function

--- server.js
+++ server.js
@@ -1394,6 +1394,15 @@ console.log(increment(5))
 // Use the Rest Operator with Function Parameters
 ///////////////////////////////////////////////////////
 
+const sum99 = (function() {
+    return function sum(x, y, z) {
+        const args = [x, y, z]
+        return args.reduce((a, b) => a + b, 0)
+    }
+})()
+
+console.log(sum99(1, 2, 3))
+
 
 
 
